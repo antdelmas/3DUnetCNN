a9bf8718716fcec6a0a3054fa6cf1dc184da24d3
antdelmas <antdelmas@gmail.com>
2021-04-29 11:02:22 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+
+import setuptools
+setuptools.setup(
+name='3dunet',
+author='Delmas Antoine',
+author_email='antdelmas@gmail.com',
+version='0.1dev',
+packages=setuptools.find_packages(),
+long_description=open('README.md').read(),
+)
